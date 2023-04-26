d6eab3a5cf6b66917ca87d1179501cd866f9d7be
Jw <jw5250@rit.edu>
2023-04-26 09:20:29 -0400

    Added more constructors to ChessConfig.java. Added comments. Extracted a method from the one that originally got all valid configurations; said separate method actually makes the configuration while the original now mainly iterates through the pieces and their potential movements. Finished ChessGUI.java and ChessPTUI.java

--- src/puzzles/chess/gui/ChessGUI.java
+++ src/puzzles/chess/gui/ChessGUI.java
@@ -1,20 +1,57 @@
 package puzzles.chess.gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.CornerRadii;
+
+
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import javafx.application.Application;
 import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import puzzles.common.Observer;
-import puzzles.chess.model.ChessModel;
-import puzzles.hoppers.model.HoppersModel;
+import javafx.stage.FileChooser;
 
+import puzzles.common.Observer;
+import puzzles.common.Coordinates;
+import puzzles.chess.model.ChessModel;
+import puzzles.chess.model.ChessConfig;
+
+/**
+ * Author:jw5250
+ * ChessGUI
+     * Design plan:
+         * Similar to PTUI
+             * Part for displaying the messages from the update
+             * Part for the board and pieces
+             * Bottom part for the load, reset, and hint buttons
+     * Current goal:
+         * Need to find a way to resize gridpane
+         * Visuals not updating properly
+     * Questions:
+         * When trying to reassign the board with new GridPane, visuals stop updating with the model.
+ *
+ * */
 public class ChessGUI extends Application implements Observer<ChessModel, String> {
     private ChessModel model;
+    private Label description;//Label that changes depending on thing done.
+    private GridPane board;//Board display.
 
     /** The size of all icons, in square dimension */
     private final static int ICON_SIZE = 75;
@@ -26,6 +63,17 @@ public class ChessGUI extends Application implements Observer<ChessModel, String
     /** The resources directory is located directly underneath the gui package */
     private final static String RESOURCES_DIR = "resources/";
 
+    //Link config characters to image links.
+    static public final HashMap<String, String> imageMap = new HashMap(){
+        {
+            put(ChessConfig.spriteToClass.BISHOP.toString(), RESOURCES_DIR+"bishop.png");
+            put(ChessConfig.spriteToClass.ROOK.toString(), RESOURCES_DIR+"rook.png");
+            put(ChessConfig.spriteToClass.PAWN.toString(), RESOURCES_DIR+"pawn.png");
+            put(ChessConfig.spriteToClass.KING.toString(), RESOURCES_DIR+"king.png");
+            put(ChessConfig.spriteToClass.KNIGHT.toString(), RESOURCES_DIR+"knight.png");
+            put(ChessConfig.spriteToClass.QUEEN.toString(), RESOURCES_DIR+"queen.png");
+        }
+    };//Quick way to turn internal model display into GUI display.
     // for demonstration purposes
     private Image bishop = new Image(getClass().getResourceAsStream(RESOURCES_DIR+"bishop.png"));
 
@@ -39,24 +87,116 @@ public class ChessGUI extends Application implements Observer<ChessModel, String
     public void init() {
         // get the file name from the command line
         String filename = getParameters().getRaw().get(0);
+        try {
+            model = new ChessModel(filename);
+            model.addObserver(this);
+        }catch(IOException i){
+            System.out.println("Invalid file: " + filename);
+        }
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
-        Button button = new Button();
-        button.setGraphic(new ImageView(bishop));
-        button.setBackground(LIGHT);
-        button.setMinSize(ICON_SIZE, ICON_SIZE);
-        button.setMaxSize(ICON_SIZE, ICON_SIZE);
-        Scene scene = new Scene(button);
-        stage.setScene(scene);
+        //System.out.println("Starting stage");
+        description = new Label();
+        description.setAlignment(Pos.CENTER);
+        board = new GridPane();
+        VBox mainContainer = new VBox();
+        mainContainer.getChildren().add(description);
+        boardReflectModel(model);
+        mainContainer.getChildren().add(board);
+        mainContainer.getChildren().add(bottomButtonPanel());
+        Scene game = new Scene(mainContainer);
+        stage.setScene(game);
         stage.show();
     }
+    /**
+     * Update the board part of the game.
+     * */
+    private void boardReflectModel(ChessModel modelCurrent){
+        if(modelCurrent == null){
+            return;
+        }//Not working properly.
+        //System.out.println("Model detected.");
+        String[][] boardRef = modelCurrent.returnCurrentBoardReference();
+        /*for(int i = 0; i < boardRef.length;i++){
+            for(int j = 0; j < boardRef[i].length;j++) {
+            System.out.print(boardRef[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+        board.getChildren().clear();
+        for(int i = 0; i < boardRef.length;i++){
+            for(int j = 0; j < boardRef[i].length;j++) {
+                Button button = new Button();
+                if( ((i+j) % 2) == 0) {
+                    button.setBackground(LIGHT);
+                }else{
+                    button.setBackground(DARK);
+                }
+                button.setMinSize(ICON_SIZE, ICON_SIZE);
+                button.setMaxSize(ICON_SIZE, ICON_SIZE);
+                if(imageMap.containsKey(boardRef[i][j])) {
+                    Image newImage = new Image(getClass().getResourceAsStream(imageMap.get(boardRef[i][j])));
+                    button.setGraphic(new ImageView(newImage));
+                }
+                button.setUserData(new Coordinates(i, j));
+                button.setOnAction(event->{
 
+                    Button source = (Button)event.getSource();
+                    Coordinates data = (Coordinates)source.getUserData();
+                    modelCurrent.select(data.row(), data.col());
+                });
+                board.add(button, j, i);
+            }
+        }
+        //System.out.println(board.getColumnCount());
+    }
+    /**
+     * Set up the bottom part of the application
+     * */
+    private HBox bottomButtonPanel(){
+        HBox layout = new HBox();
+        Button loadButton = new Button();
+        loadButton.setText("Load");
+        loadButton.setOnAction(event->{
+            //System.out.println("Load");
+            FileChooser chooser = new FileChooser();
+            String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
+            currentPath += File.separator + "data" + File.separator + "chess";  // or "hoppers"
+            chooser.setInitialDirectory(new File(currentPath));
+            File file = chooser.showOpenDialog(stage);
+            String filename = "data" + File.separator + "chess" + File.separator;
+            if(file != null) {
+                filename += file.getName();
+            }
+            model.load(filename);
+        });
+        layout.getChildren().add(loadButton);
+
+        Button resetButton = new Button();
+        resetButton.setText("Reset");
+        resetButton.setOnAction(event->{
+            //System.out.println("Reset");
+            model.reset();
+        });
+        layout.getChildren().add(resetButton);
+        Button hintButton = new Button();
+        hintButton.setText("Hint");
+        hintButton.setOnAction(event->{
+            //System.out.println("Hint");
+            model.hint();
+        });
+        layout.getChildren().add(hintButton);
+        layout.setAlignment(Pos.CENTER);
+        return layout;
+    }
     @Override
-    public void update(ChessModel chessModel, String msg) {
-
+    public void update(ChessModel chessModel, String msg) {//Not getting called. Why?
+        //System.out.println("Called update!");
+        description.setText(msg);
+        boardReflectModel(chessModel);//Not updating the grid
         this.stage.sizeToScene();  // when a different sized puzzle is loaded
     }
 
--- src/puzzles/chess/model/ChessConfig.java
+++ src/puzzles/chess/model/ChessConfig.java
@@ -2,7 +2,6 @@ package puzzles.chess.model;
 
 import puzzles.common.solver.Configuration;
 import puzzles.common.Coordinates;
-
 import java.io.IOException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -12,14 +11,15 @@ import java.util.HashMap;
 import java.util.Arrays;
 /**
  * ChessConfig.java
- * Notes:
-     * Problems:
-         * Pieces should return a set of potential movements.
+ * Author:jw5250
  * */
 public class ChessConfig implements Configuration {
     private String[][] board;//The board.
     private HashMap<Coordinates, chessPiece> pieces;//The list of pieces.
-    private enum spriteToClass{
+    /**
+     * Enum to avoid magic strings
+     * */
+    public enum spriteToClass{
         KNIGHT("N"),
         BISHOP("B"),
         QUEEN("Q"),
@@ -35,7 +35,7 @@ public class ChessConfig implements Configuration {
             return spr;
         }
     };
-    //Seems to be working.
+
     public ChessConfig(String filename) throws IOException {
         Scanner fileReader = new Scanner(new FileReader(filename));
         String[] rawDimensionData = fileReader.nextLine().split(" ");
@@ -43,47 +43,20 @@ public class ChessConfig implements Configuration {
         int rows = Integer.parseInt(rawDimensionData[0]);
         int cols = Integer.parseInt(rawDimensionData[1]);
         board = new String[rows][cols];
-        for(int i = 0; i < rows; i++){
+        for(int i = 0; i < board.length; i++){
             String[] spriteData = fileReader.nextLine().split(" ");
-            for(int j = 0; j < cols; j++){
+
+            for(int j = 0; j < board[i].length; j++){
                 board[i][j] = spriteData[j];
                 addPotentialPiece(i, j, spriteData[j]);
             }
         }
         fileReader.close();
     }
-    public void printData(){
-        //System.out.println("List of pieces:");
-        //System.out.println(pieces.toString());
-        //System.out.println("Amount of pieces:" + pieces.size());
-        //System.out.println("Board:");
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
-    private void addPotentialPiece(int row, int col, String s){
-        if(s.equals(spriteToClass.KING.toString())){
-            pieces.put(new Coordinates(row, col), new king(new Coordinates(row, col), s));
-        }else if(s.equals(spriteToClass.QUEEN.toString())){
-            pieces.put(new Coordinates(row, col), new queen(new Coordinates(row, col), s));
-        }else if(s.equals(spriteToClass.PAWN.toString())){
-            pieces.put(new Coordinates(row, col), new pawn(new Coordinates(row, col), s));
-        }else if(s.equals(spriteToClass.KNIGHT.toString())){
-            pieces.put(new Coordinates(row, col), new knight(new Coordinates(row, col), s));
-        }else if(s.equals(spriteToClass.BISHOP.toString())){
-            pieces.put(new Coordinates(row, col), new bishop(new Coordinates(row, col), s));
-        }else if(s.equals(spriteToClass.ROOK.toString())){
-            pieces.put(new Coordinates(row, col), new rook(new Coordinates(row, col), s));
-        }
-    }
     public ChessConfig(ChessConfig other){
         board = new String[other.board.length][other.board[0].length];
         for(int i = 0; i < board.length;i++){
-            for(int j = 0; j < board.length;j++){
+            for(int j = 0; j < board[i].length;j++){
                 board[i][j] = other.board[i][j];
             }
         }
@@ -102,10 +75,50 @@ public class ChessConfig implements Configuration {
 
         pieces = new HashMap<>();
         for(Coordinates coord : thePieces.keySet()){
-         pieces.put(coord, thePieces.get(coord));
+            pieces.put(coord, thePieces.get(coord));
+        }
+    }
+    /**
+     * Used originally for testing if the board data is being initialized correctly. Legacy code.
+     * */
+    public void printData(){
+        //System.out.println("List of pieces:");
+        //System.out.println(pieces.toString());
+        //System.out.println("Amount of pieces:" + pieces.size());
+        //System.out.println("Board:");
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    /**
+     * Pseudo-factory method that adds pieces to the reference of pieces to board positions.
+     * @param row the row
+     * @param col the column
+     * @param s the visual representation of the piece.
+     * */
+    private void addPotentialPiece(int row, int col, String s){
+        if(s.equals(spriteToClass.KING.toString())){
+            pieces.put(new Coordinates(row, col), new king(new Coordinates(row, col), s));
+        }else if(s.equals(spriteToClass.QUEEN.toString())){
+            pieces.put(new Coordinates(row, col), new queen(new Coordinates(row, col), s));
+        }else if(s.equals(spriteToClass.PAWN.toString())){
+            pieces.put(new Coordinates(row, col), new pawn(new Coordinates(row, col), s));
+        }else if(s.equals(spriteToClass.KNIGHT.toString())){
+            pieces.put(new Coordinates(row, col), new knight(new Coordinates(row, col), s));
+        }else if(s.equals(spriteToClass.BISHOP.toString())){
+            pieces.put(new Coordinates(row, col), new bishop(new Coordinates(row, col), s));
+        }else if(s.equals(spriteToClass.ROOK.toString())){
+            pieces.put(new Coordinates(row, col), new rook(new Coordinates(row, col), s));
         }
     }
 
+    /**
+     * Check if a config is a solution.
+     * */
     @Override
     public boolean isSolution() {
         //Solution is when only one piece remains.
@@ -131,11 +144,17 @@ public class ChessConfig implements Configuration {
         return false;
 
     }
+    /**
+     * For avoiding duplicate hashcode keys. As the only guaranteed difference between the Configs is the board,
+     * use a deep hash code on the 2d array.
+     * */
     @Override
     public int hashCode(){
         return Arrays.deepHashCode(board);
     }
-
+    /**
+     * Convert Config into toString form.
+     * */
     @Override
     public String toString(){
         StringBuilder s = new StringBuilder();
@@ -147,6 +166,9 @@ public class ChessConfig implements Configuration {
         }
         return s.toString();
     }
+    /**
+     * Get neighbor configurations.
+     * */
     @Override
     public Collection<Configuration> getNeighbors() {
         ArrayList<Configuration> configs = new ArrayList<>();
@@ -157,6 +179,7 @@ public class ChessConfig implements Configuration {
             }
             //System.out.println("Available moves:" + p + ": " + newCoords);
             for(int i = 0; i < newCoords.size();i++) {
+                /*
                 String[][] newBoard = new String[board.length][board[0].length];
                 for(int j = 0; j < board.length; j++){
                     for(int k = 0; k < board[j].length;k++){
@@ -172,13 +195,46 @@ public class ChessConfig implements Configuration {
                 chessPiece newPiece = p.clone();
                 newPiece.changeCoordinates(newCoords.get(i));//Get new piece's coordinates changed.
                 newPieces.put(newCoords.get(i), newPiece);//Change new location of p.
-                newPieces.remove(p.getCoordinates());
-                ChessConfig newConfig = new ChessConfig(newBoard, newPieces);
+                newPieces.remove(p.getCoordinates());*/
+                //ChessConfig newConfig = new ChessConfig(newBoard, newPieces);
+                ChessConfig newConfig = configAfterMovement(newCoords.get(i), p);
                 configs.add(newConfig);
             }
         }
-        //System.out.println("Number of configurations:" + configs.size());
         return configs;
+    }
+    /**
+     * The configuration after a piece at some position is moved.
+     * @param newCoords the new place the piece would move
+     * @param p the piece to be moved in question.
+     * */
+    public ChessConfig configAfterMovement(Coordinates newCoords, chessPiece p){
+        String[][] newBoard = new String[board.length][board[0].length];
+        for(int j = 0; j < board.length; j++){
+            for(int k = 0; k < board[j].length;k++){
+                newBoard[j][k] = board[j][k];
+            }
+        }
+        newBoard[p.getCoordinates().row()][p.getCoordinates().col()] = chessPiece.EMPTY;
+        newBoard[newCoords.row()][newCoords.col()] = p.getSprite();
+        HashMap<Coordinates, chessPiece> newPieces = new HashMap<>();
+        for (Coordinates c : pieces.keySet()) {
+            newPieces.put(c, pieces.get(c));
+        }
+        chessPiece newPiece = p.clone();
+        newPiece.changeCoordinates(newCoords);//Get new piece's coordinates changed.
+        newPieces.put(newCoords, newPiece);//Change new location of p.
+        newPieces.remove(p.getCoordinates());
+        return new ChessConfig(newBoard, newPieces);
+    }
+    public Collection<Coordinates> getValidPositionsOfAPiece(Coordinates coords){
+        return pieces.get(coords).moveAndFindPiece(board);
+    }
+    public String[][] returnBoardReference(){
+        return board;
+    }
+    public chessPiece getPieceAtCoordinates(Coordinates key){
+        return pieces.get(key);
     }
 
     public static void main(String[] s){
